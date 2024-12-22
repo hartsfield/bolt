6eb2fa3626390a4b1707666c97755089e4c94855
John Hartsfield <john@unilink.local>
2024-12-21 22:04:44 -0500

    updates

--- structure/autoload.sh
+++ structure/autoload.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 pkill -9 $1
 go build -o $1
+
+# linux
 servicePort=$2 logFilePath=./logfile.txt ./$1 &>> logfile.txt &
+
+# mac os
+# servicePort=$2 logFilePath=./log.txt ./$1 >> log.txt 2>&1 &
