4276d42a9c36292b51cf8135a69dc0d73f5c7fcc
John Hartsfield <john@unilink.local>
2025-01-07 04:06:11 -0500

    updates

--- test_tools/testproject.sh
+++ test_tools/testproject.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
+if [ -d "testApp" ]; then          # check for an old testApp dir and delete it
+  rm -rf testApp
+fi
 
-mkdir testApp && cd testApp        # make an empty directoy and cd into
+mkdir testApp && cd testApp        # make an empty testApp dir and cd into it
 
-bolt                               # initiate app
+bolt                               # initiate bolt app
 bolt --new-component=test          # create a component called test
 bolt --insert-component=test,main  # insert test into the main page
-bolt --autonav about,merch,contact # create a nav bar and insert the necessary components
+bolt --autonav about,merch,contact # automatically create a nav bar with components
 
 echo '{
     "textarea": ["MyText"],
     "file": ["Media"],
     "text": ["Title", "Email"]
-}' >> model.json                   # echo the data into a file "model.json"
+}' >> model.json                   # echo a data structure into a file called "model.json"
 bolt --streamable model.json       # use model.json to create the form and data stream
-
 ./autoload.sh                      # run the app
 cd .. && go run main.go            # cd back up to the test dir and run the test
